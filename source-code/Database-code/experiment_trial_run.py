352df521a196749578331e82bd680d27da67cbc2
Sophie Shapcott <ShapcottSM@cardiff.ac.uk>
2019-10-31 15:23:52 +0000

    Trialling with vertex enumeration
    Experiment code is currently being adapted to run with vertex
    enumeration. Not running completely accurately yet.

--- source-code/Database-code/experiment_trial_run.py
+++ source-code/Database-code/experiment_trial_run.py
@@ -5,9 +5,17 @@ stochasticity = np.linspace(0, 1, 11)
 prob_of_game_end = np.linspace(0.001, 1-0.001, 5)
 experiment_number = 1
 
-expfun.create_database("data/support_enumeration/")
+#expfun.create_database("data/support_enumeration/")
+#for noise in stochasticity:
+
+    #expfun.run_experiment(max_num_of_opponents=4, tournament_repeats=10, game_ending_probs=prob_of_game_end, seed=123, noise=noise, database_filepath="data/support_enumeration/", num_of_opponents=1, experiment_num=experiment_number, support_enumeration=True)
+    
+   # experiment_number += 20
+
+
+expfun.create_database("data/vertex_enumeration/")
 for noise in stochasticity:
 
-    expfun.run_experiment(max_num_of_opponents=4, tournament_repeats=10, game_ending_probs=prob_of_game_end, seed=123, noise=noise, database_filepath="data/support_enumeration/", num_of_opponents=1, experiment_num=experiment_number, support_enumeration=True)
+    expfun.run_experiment(max_num_of_opponents=4, tournament_repeats=10, game_ending_probs=prob_of_game_end, seed=123, noise=noise, database_filepath="data/vertex_enumeration/", num_of_opponents=1, experiment_num=experiment_number, support_enumeration=False)
     
     experiment_number += 20
