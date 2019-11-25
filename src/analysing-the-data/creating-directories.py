8f01b4f08bb75ce095bd30235e2efa85a97c43c2
Sophie Shapcott <ShapcottSM@cardiff.ac.uk>
2019-11-25 18:44:37 +0000

    Recoding the creation of directories

--- src/analysing-the-data/creating-directories.py
+++
@@ -1,8 +0,0 @@
-from pathlib import *
-p = Path("../../images/folk_thm")
-if p.exists() == False:
-    p.mkdir(parents=True)
-    print("Path ../../images/folk_thm was created.")
-else:
-    print("Path ../../images/folk_thm already exists.")
-
