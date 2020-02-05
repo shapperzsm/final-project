fe6199e7a7e0b4735e7959546d9a817d01bf5370
Sophie Shapcott <ShapcottSM@cardiff.ac.uk>
2020-02-05 20:25:24 +0000

    Cleaning up src to debug and run again.

---
+++ src/database_code/__init__.py
@@ -0,0 +1,1 @@
+from database_code.experiment_functions import *
--- tests/unit/test_experiment_functions.py
+++ tests/unit/test_experiment_functions.py
@@ -1,4 +1,4 @@
-import experiment_functions as expfun
+import database_code as dbcode
 import axelrod as axl
 import nashpy as nash
 import numpy as np
@@ -6,14 +6,14 @@ import numpy as np
 
 def test_who_is_playing():
     for num_of_opponents in range(1, 10):
-        players = expfun.who_is_playing(num_of_opponents)
+        players = dbcode.who_is_playing(num_of_opponents)
         assert len(players) == num_of_opponents + 1
         for player in players:
             assert player.classifier["long_run_time"] == False
 
 
 def test_get_game():
-    game = expfun.get_game(
+    game = dbcode.get_game(
         4, [axl.Cooperator(), axl.TitForTat(), axl.Defector()], 0.2, 0.7
     )
     type(game) == type(dict())
@@ -26,7 +26,7 @@ def test_get_game():
 def test_get_prob_of_defection():
     matrix = np.array([[3, 0], [5, 1]])
     nash_eq = list(nash.Game(matrix, matrix.transpose()).support_enumeration())
-    defection_prob = expfun.get_prob_of_defection(np.array([[3, 0], [5, 1]]))
+    defection_prob = dbcode.get_prob_of_defection(np.array([[3, 0], [5, 1]]))
     assert len(defection_prob["nash equilibria"]) == len(nash_eq)
     assert defection_prob["least prob of defect"] == min(
         [sigma_1[-1] for sigma_1, _ in nash_eq]
@@ -38,6 +38,6 @@ def test_get_prob_of_defection():
 
 
 def test_array_to_string():
-    string_array = expfun.array_to_string(np.array([[3, 0], [5, 1]]))
+    string_array = dbcode.array_to_string(np.array([[3, 0], [5, 1]]))
     assert type(string_array) == type("this is a test")
     assert string_array == "3 0 5 1"
